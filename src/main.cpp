ad539f204d223e74efcb54b219ad218893d76428
RumCoy <309020109+RumCoy@users.noreply.github.com>
2026-08-10 18:27:17 -0700

    Fix SKSE logging

--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,9 @@
 
 #include <filesystem>
 #include <SKSE/SKSE.h>
-#include <spdlog/sinks/basic_file_sink.h>
 
 namespace
 {
-    void InitializeLogging()
-    {
-        const auto directory = SKSE::log::log_directory();
-        if (!directory) {
-            SKSE::stl::report_and_fail("Unable to locate the SKSE log directory");
-        }
-
-        auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
-            (*directory / "VariousDialogueTags.log").string(), true);
-        auto logger = std::make_shared<spdlog::logger>("global", std::move(sink));
-#ifndef NDEBUG
-        logger->set_level(spdlog::level::debug);
-#else
-        logger->set_level(spdlog::level::info);
-#endif
-        logger->flush_on(spdlog::level::info);
-        spdlog::set_default_logger(std::move(logger));
-    }
-
     void OnSKSEMessage(SKSE::MessagingInterface::Message* message)
     {
         if (!message || message->type != SKSE::MessagingInterface::kDataLoaded) {
@@ -59,7 +39,6 @@ namespace
 
 SKSEPluginLoad(const SKSE::LoadInterface* skse)
 {
-    InitializeLogging();
     SKSE::Init(skse);
 
     VariousDialogueTags::DialogueMenuHook::Install();
